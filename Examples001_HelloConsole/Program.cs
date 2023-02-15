92c927790023d20e4a1d0da85658af3cd8ab0291
Saidov <shama14121989@gmail.com>
2023-02-15 13:17:52 +0300

    Examples002_HelloUser

--- Examples001_HelloConsole/Program.cs
+++ Examples001_HelloConsole/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine(" 2 Hello, World!");
+﻿
+string username = Console.Readline();
+Console.WriteLine("Привет,");
+Console.WriteLine(username);
